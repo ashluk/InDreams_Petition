332c4f8d139aa8ee0543863f2fe31c2ee26afa97
Ash Gowriluk <ashleerluk@gmail.com>
2021-02-17 16:02:24 +0100

    4:02pm

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+const hb = require("express-handlebars");
+app.engine("handlebars", hb());
+app.set("view engine", "handlebars");
+
+//app.use(express.static(__dirname + "/static"));
 
 console.log("db", db);
+app.use(express.static("./public"));
 
-app.use(express.static("public"));
+//app.use(express.static(__dirname + "./public"));
 
 app.get("/petition", (req, res) => {
     res.render("/petition", {});
 });
+/*app.post("/petition", (req, res) => {
+    const { first, last } = req.body;
+    console.log("requested body", req.body);
+});*/
 
+app.get("/signers", (req, res) => {
+    res.render("/signers", {});
+});
+app.get("/thanks", (req, res) => {
+    res.render("/thanks", {});
+});
 app.listen(8080, () => console.log("Petition up and running...."));
 
 /*db.addCity("York", "UK", 500000)
