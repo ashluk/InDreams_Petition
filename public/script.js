87a6fcfa4bb75611a4874b515bcc29c038857994
Ash Gowriluk <ashleerluk@gmail.com>
2021-02-18 08:51:39 +0100

    8:50am nothing working yet

--- public/script.js
+++ public/script.js
@@ -1,1 +1,71 @@
 //console.log($'hello');
+const signature = document.getElementById("signature");
+const context = signature.getContext("2d");
+var dataURL;
+/*const clear = document.getElementById("clear");*/
+//const submit = document.getElementById("submit");
+
+let moving = false;
+let x = 0;
+let y = 0;
+
+signature.addEventListener("mousedown", (e) => {
+    x = e.offsetX;
+    y = e.offsetY;
+
+    moving = true;
+});
+
+signature.addEventListener("mousemove", (e) => {
+    if (moving === true) {
+        drawLine(context, x, y, e.offsetX, e.offsetY);
+        x = e.offsetX;
+        y = e.offsetY;
+    }
+});
+
+window.addEventListener("mouseup", (e) => {
+    if (moving === true) {
+        drawLine(context, x, y, e.offsetX, e.offsetY);
+        x = 0;
+        y = 0;
+
+        moving = false;
+        var dataUrl = signature.toDataURL();
+        console.log("this is my dataURL", dataURL);
+    }
+});
+
+function drawLine(context, x1, y1, x2, y2) {
+    context.beginPath();
+    context.strokeStyle = "black";
+    context.lineWidth = 1;
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.stroke();
+    context.closePath();
+}
+
+/*submit.addEventListener("click", function post(event) {
+    if (signature.isEmpty()) {
+        alert("Please provide signature first.");
+    } else {
+        document.body.innerHTML +=
+            '<form id="form" action="newpage.php" method="post"><input type="hidden" name="image" value="' +
+            signature.toDataURL() +
+            '"></form>';
+        document.getElementById("form").submit();
+    }
+});*/
+/*clear.addEventListener("click", function (e) {
+    console.log("i clicked cledar");
+    signature.clear();
+});
+submit.addEventListener("click", function (e) {
+    console.log("i clicked submit");
+    if (signature.isEmpty()) {
+        alert("Please provide signature first.");
+    } else {
+        window.open(signature.toDataURL());
+    }
+});*/
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const db = require("./db");
 const hb = require("express-handlebars");
+
 app.engine("handlebars", hb());
 app.set("view engine", "handlebars");
 
@@ -9,22 +10,31 @@ app.set("view engine", "handlebars");
 
 console.log("db", db);
 app.use(express.static("./public"));
+app.use(
+    express.urlencoded({
+        extended: false,
+    })
+);
 
 //app.use(express.static(__dirname + "./public"));
 
 app.get("/petition", (req, res) => {
-    res.render("/petition", {});
+    res.render("petition", {});
+    /* if (user_first === !undefined && user_last === !undefined) {
+        res.redirect("/thanks");
+    }*/
 });
-/*app.post("/petition", (req, res) => {
-    const { first, last } = req.body;
+
+app.post("/petition", (req, res) => {
+    const { first, last, signature } = req.body;
     console.log("requested body", req.body);
-});*/
+});
 
 app.get("/signers", (req, res) => {
-    res.render("/signers", {});
+    res.render("signers", {});
 });
 app.get("/thanks", (req, res) => {
-    res.render("/thanks", {});
+    res.render("thanks", {});
 });
 app.listen(8080, () => console.log("Petition up and running...."));
 
